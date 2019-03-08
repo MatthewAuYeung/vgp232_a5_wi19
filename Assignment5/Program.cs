12635bd7ea63e1fd1e0f8e43006ff5cf10d5e615
chong921130 <chong921130@gmail.com>
2019-03-08 01:39:44 -0800

    d

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -45,43 +45,6 @@ namespace Assignment5
                 }
             }
 
-
-            // TODO: hook up item data to display with the inventory
-
-            var source = new Inventory()
-            {
-                ItemToQuantity =
-                    new Dictionary<object, object> { { "Poke ball", 10 }, { "Potion", 10 } }
-            };
-
-            // TODO: move this into a inventory with a serialize and deserialize function.
-            string inventoryFile = "inventory.xml";
-            using (var writer = XmlWriter.Create(inventoryFile))
-                (new XmlSerializer(typeof(Inventory))).Serialize(writer, source);
-
-            using (var reader = new StreamReader(inventoryFile))
-            {
-                var serializer = new XmlSerializer(typeof(Inventory));
-                try
-                {
-                    Inventory inventory = serializer.Deserialize(reader) as Inventory;
-                    if (inventory != null)
-                    {
-                        foreach (var item in inventory.ItemToQuantity)
-                        {
-                            Console.WriteLine("Item: {0} Quantity: {1}", item.Key, item.Value);
-                        }
-                    }
-                }
-
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Cannot load {0} due to the following {1}",
-                        inventoryFile, ex.Message);
-                }
-
-            }
-
             Inventory myObject;
             XmlSerializer mySerializer = new XmlSerializer(typeof(Inventory));
             FileStream myFileStream = new FileStream("inventory.xml", FileMode.Open);
